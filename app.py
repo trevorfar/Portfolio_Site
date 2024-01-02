bc5c694f42b11a4e0d6d4d12cb1a376941eef707
Trevor Farias <levelonethousandr@gmail.com>
2024-01-01 20:30:36 -0500

    Working Everything

--- app.py
+++ app.py
@@ -94,3 +94,11 @@ def delete(id):
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/app1')
+def app1():
+    return render_template('app1.html')
+
+@app.route('/app2')
+def app2():
+    return render_template('app2.html')
