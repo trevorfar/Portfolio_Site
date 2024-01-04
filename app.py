a55aa45bf29e0f4bf5dfb5e0438084b35eacf851
Trevor Farias <levelonethousandr@gmail.com>
2024-01-04 01:26:12 -0500

    basic structure of tic tac toe done, no algorithyms

--- app.py
+++ app.py
@@ -61,57 +61,6 @@ def post(post_id):
     return render_template('post.html', post=post)
 
 
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-    return render_template('create.html')
-
-
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-
-    return render_template('edit.html', post=post)
-
-
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
-
-
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -149,6 +98,6 @@ def calculate():
     return render_template('calculator.html', result=result, expression=expression,
         expHistory=expHistory, resHistory=resHistory)
 
-@app.route('/app2')
-def app2():
-    return render_template('app2.html') 
+@app.route('/ticTacToe')
+def ticTacToe():
+    return render_template('ticTacToe.html') 
--- static/java/parallax.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('scroll', function() {
-    const parallaxSection = document.querySelector('.parallax-section');
-    const scrolled = window.scrollY;
-    parallaxSection.style.transform = `translateY(${scrolled * 0.5}px)`; // Adjust the multiplier for different speed
-});
