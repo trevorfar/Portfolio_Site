3f5a1a1e72eed09c37f313d8bd11653ce88631f4
Trevor Farias <levelonethousandr@gmail.com>
2024-01-07 00:13:19 -0500

    Working tictactoe, no html or css, still textare all logic is good.

--- static/java/tictactoe.js
+++ static/java/tictactoe.js
@@ -13,7 +13,7 @@ function makeMove(index){
         board[index] = currentPlayer;   
         updateBoard();
         swapPlayer();
-        // checkWinner();
+        checkWinner();
         updateDisplay();
     }
 }
@@ -30,6 +30,26 @@ function swapPlayer() {
     currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
 }
 
+function checkWinner(){
+     if ((board[2] !== '' && board[4] !== '' && board[6] !== '' && board[2] == board[4] && board[2] == board[6])){
+        alert("HELLO");
+    }
+    else if ((board[0] !== '' && board[4] !== '' && board[8] !== '' && board[0] == board[4] && board[0] == board[8])){
+        alert("HELLO");
+    }
+    else{
+        for(let i = 0; i < board.length; i ++){ 
+            if ((board[i] !== '' && board[i+1] !== '' && board[i+2] !== '' && board[i] == board[i+1] && board[i] == board[i+2])){
+                alert("HELLO");
+            }  
+            else if ((board[i] !== '' && board[i+3] !== '' && board[i+6] !== '' && board[i] == board[i+3] && board[i] == board[i+6])){
+                alert("HELLO");
+            }
+            
+            
+    }
+}
+}
 
 function updateDisplay() {
     document.getElementById('display').value = board;
