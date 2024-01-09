bd7f0e15edb905f567249d0a8e2ee336f02d45a5
Trevor Farias <levelonethousandr@gmail.com>
2024-01-09 11:58:47 -0500

    Working parallax

--- schema.sql
+++ schema.sql
@@ -1,7 +1,5 @@
--- Drop the table if it exists (for testing purposes)
 DROP TABLE IF EXISTS tic_tac_toe_boards;
 
--- Create the tic_tac_toe_boards table
 CREATE TABLE tic_tac_toe_boards (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     board_data TEXT
