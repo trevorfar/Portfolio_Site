7ee7beff5b4074865d6ce30fa683cce744158b43
Trevor Farias <levelonethousandr@gmail.com>
2024-01-11 11:05:49 -0500

    Working slightly

--- app.py
+++ app.py
@@ -40,30 +40,14 @@ def clearHistory():
 currentPlayer = 'X'
 
 
-def get_post(post_id): 
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
 
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
-
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
 
 
 @app.route('/about')
@@ -107,4 +91,3 @@ def calculate():
 @app.route('/ticTacToe')
 def ticTacToe():
     return render_template('ticTacToe.html', )
-
