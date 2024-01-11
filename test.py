4c1d73bc67698eda976d9dbccc75a7f3cebdab22
trevorfar <levelonethousandr@gmail.com>
2024-01-10 21:09:27 -0500

    test

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-class Animals:
-    def speak():
-        print("I Dont Make Noise")
-
-
-class dog(Animals):
-    def speak():
-        print("bark")
-
-
-
-
-d = dog.speak()
-a = Animals.speak()
+def testing():
+    print("hello")
