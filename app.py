055926750780647219069bd22c85039a94ca4a43
Trevor Farias <levelonethousandr@gmail.com>
2024-01-22 11:33:19 -0500

    test

--- app.py
+++ app.py
@@ -20,33 +20,6 @@ resHistory = []
 
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Student(db.Model):
-    age = db.Column(db.Integer, primary_key=True)
-    bio = db.Column(db.Text)
-
-    def __repr__(self):
-        return f'<Student {self.firstname}>'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def evaluateExpression(expression):
     try:
         if not expression.strip():
